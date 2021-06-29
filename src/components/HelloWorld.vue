5c84afb4068ee81fc021749f6c67376cc4449408
이재영 <giver12@naver.com>
2021-06-29 14:14:57 +0900

    Update: 자동 배포 실패 테스트 커밋

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>props 미사용으로 인한 테스트 실패 test</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
